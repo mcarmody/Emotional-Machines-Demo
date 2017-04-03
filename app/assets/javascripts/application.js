012bf7ecdf607ff70972cb46f2599f48049d43c6
Mike <mcarmody@productiveedge.com>
2017-04-03 11:00:15 -0400

    update alert count and temp with click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,42 +40,59 @@ $(document).ready(function() {
 	$('.alertButton').click(function() {
 
 
-		var currentTime = Math.round((new Date).getTime() / 1000);
+		var currentTime = Math.round((today).getTime() / 1000);
 		var limit = 1200 //1200 is every data point within the past 5 hours
 		var fiveHoursAgo = currentTime-(limit / 240 * 60 * 60);
+		var highAlertHTML = $(this).siblings(".highAlerts");
+		var lowAlertHTML = $(this).siblings(".lowAlerts");
+		var tempHTML = $(this).siblings(".elementDatum");	
+		var highAlertCounter = 0;
+		var lowAlertCounter = 0;
+		
 
-		$.getJSON("https://api.elementalmachines.io/api/machines/8092d98c-b92f-4343-a8ae-104f90362de8/samples.json?access_token=7eb3d0a32f2ba1e8039657ef2bd1913d95707ff53e37dfd0344ac62ded3df033&from="+fiveHoursAgo+"&limit="+limit, function(data) {
+		$.ajax({
+			url: "https://api.elementalmachines.io/api/machines/8092d98c-b92f-4343-a8ae-104f90362de8/samples.json?access_token=7eb3d0a32f2ba1e8039657ef2bd1913d95707ff53e37dfd0344ac62ded3df033&from="+fiveHoursAgo+"&limit="+limit,
+			dataType: 'json',
+			async: true,
+			success: function(data) {
 			
-			//check to make sure we can receive and parse the API data at all
-			console.log("Current temperature is " + data[data.length-1].tempextcal + ", logged at: " + data[data.length-1].sample_date);
+				//check to make sure we can receive and parse the API data at all
+				console.log("Current temperature is " + data[data.length-1].tempextcal + ", logged at: " + data[data.length-1].sample_date);
+				temp = data[data.length-1].tempextcal;
+				console.log(temp);
 
-			var i;
+				var i;
 
-			for (i = 0; i < data.length; i++) {
-				temp = data[i].tempextcal;
-				timeStamp = data[i].sample_epoch;
-				date = data[i].sample_date;
+				for (i = 0; i < data.length; i++) {
+					loopTemp = data[i].tempextcal;
+					loopTimeStamp = data[i].sample_epoch;
+					loopDate = data[i].sample_date;
 
-				// high temp alert check
-				if (Math.abs(temp) < Math.abs(highAlert)) {
-					highAlertCounter++;
-					//console.log(temp);
+					// high temp alert check
+					if (Math.abs(loopTemp) < Math.abs(highAlert)) {
+						highAlertCounter++;
+						//console.log(temp);
+					};
+
+					// low temp alert check
+					if (Math.abs(loopTemp) > Math.abs(lowAlert)) {
+						lowAlertCounter++;
+						//console.log(temp);
+					};
+
+					if (Math.abs(loopTemp) > maxTemp) {
+						maxTemp = data[i].tempextcal;
+					};
 				};
 
-				// low temp alert check
-				if (Math.abs(temp) > Math.abs(lowAlert)) {
-					lowAlertCounter++;
-					//console.log(temp);
-				};
-
-				if (Math.abs(temp) > maxTemp) {
-					maxTemp = data[i].tempextcal;
-				};
-			};
-
-			console.log("done, data length: " + data.length);
-			console.log("There have been " + highAlertCounter + " high-temp alerts and " + lowAlertCounter + " low alerts in the past " + limit / 240 + " hours.");
-
+				console.log("done, data length: " + data.length);
+				console.log("There have been " + highAlertCounter + " high-temp alerts and " + lowAlertCounter + " low alerts in the past " + limit / 240 + " hours.");
+			}
+		}).done( function() {
+			//update the page text
+			highAlertHTML.html(highAlertCounter);
+			lowAlertHTML.html(lowAlertCounter);
+			tempHTML.html(temp);
 		});
 
 
